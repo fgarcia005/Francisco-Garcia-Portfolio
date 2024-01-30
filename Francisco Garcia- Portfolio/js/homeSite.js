bdc1e88c147b131740925acc0092d1886fb2b2d2
fgarcia005 <fgarcia@student.neumont.edu>
2024-01-30 14:43:28 -0700

    portfolio uploaded

---
+++ Francisco Garcia- Portfolio/js/homeSite.js
@@ -0,0 +1,42 @@
+//checking
+console.log("Hope is here");
+
+//variables
+let img = 0;
+let imgs = ["subImages/img1.jpg","subImages/img2.jpg","subImages/img3.jpg","subImages/img4.jpg"];
+var data;
+
+//DOM
+const slide = document.getElementById("slideshow");
+const quote = document.getElementById("quote");
+
+//code start here
+slide.src = imgs[img]
+setInterval(slideshow, 5000);
+
+//API
+const api_url ="https://type.fit/api/quotes";
+
+async function getapi(url)
+{
+  const response = await fetch(url);
+  data = await response.json();
+  console.log(data);
+}
+
+getapi(api_url);
+
+// functions
+function slideshow(){
+    if (img == imgs.length){
+        img = 0;
+    }
+    slide.src = imgs[img];
+    img++;
+}
+
+function displayQuote(){
+    quote
+}
+
+
---
+++ Francisco Garcia- Portfolio/js/skillSite.js
@@ -0,0 +1,2 @@
+//checking
+console.log("Hope is here");
